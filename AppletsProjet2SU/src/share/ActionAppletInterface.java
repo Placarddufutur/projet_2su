2a5350c99626e989f3412c1cef537627df67da25
nath014 <51096316+nath014@users.noreply.github.com>
2023-02-09 16:39:12 +0100

    Delete AppletsProjet2SU directory

--- AppletsProjet2SU/src/share/ActionAppletInterface.java
+++
@@ -1,7 +0,0 @@
-package share;
-import javacard.framework.APDU;
-import javacard.framework.Shareable;
-
-public interface ActionAppletInterface extends Shareable {
-	public boolean sendACRRequest(APDU apdu);
-}
